61bf7b276f3acb3fe48025dc096de0fcd44ccb5b
M ALI RAZA <itsaaliraza@gmail.com>
2025-05-01 14:49:33 +0500

    auto login inserted

--- crawler.py
+++ crawler.py
@@ -1,7 +1,5 @@
-
-num_of_pages=2   #crawling links of first 2 pages
-from botasaurus.request import request, Request
-from botasaurus.soupify import soupify
+import random
+from botasaurus.browser import browser, Driver
 import csv
 from urllib.parse import urljoin
 import os
@@ -9,10 +7,6 @@ import threading
 import logging
 from datetime import datetime
 import time
-import random
-
-delay = random.uniform(2, 5)
-time.sleep(delay)
 
 os.makedirs("logs", exist_ok=True)
 os.makedirs("output", exist_ok=True)
@@ -25,85 +19,72 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
 write_lock = threading.Lock()
-output_file = "output/rings_urls.csv"
+output_file = "output/crawled_urls.csv"
 base_url = "https://www.therealreal.com/"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.9",
+proxy_data = {
+    'http': 'http://VCrI40bs0ca4Jhy0:AGCuQwc4rHWltmlJ@geo.iproyal.com:12321',
+    'https': 'http://VCrI40bs0ca4Jhy0:AGCuQwc4rHWltmlJ@geo.iproyal.com:12321'
 }
 
-proxy_data = {
-    "http": "http://a91ac6e8a76376a0:RNW78Fm5@res.proxy-seller.com:10000",
-    "https": "http://a91ac6e8a76376a0:RNW78Fm5@res.proxy-seller.com:10000",
-}
 
 def write_to_csv(link):
     with write_lock:
         with open(output_file, mode="a", newline="", encoding="utf-8") as file:
-                writer = csv.writer(file)
-                writer.writerow([link])
+            writer = csv.writer(file)
+            writer.writerow([link])
 
 
-# Initialize CSV file with header
 with open(output_file, mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["RINGS_URLS"])
 
 
-
-
-@request(cache=False,
-    parallel=10,
-    max_retry=20,
-    close_on_crash=True,
-    raise_exception=True,
-    create_error_logs=False,
-    output=None,)
-def crawl_rings(request: Request, data):
-    page_number=data["page_number"]
-    ring_links=[]
+@browser
+def scrape_data(driver: Driver, data):
     try:
-        delay = random.uniform(3, 8)
-        time.sleep(delay)
-        response = request.get(
-            f"https://www.therealreal.com/sales/designer-jewelry?after=YXJyYXljb25uZWN0aW9uOjIzNQ%3D%3D&first=120&page={page_number}&taxons%5B%5D=1099",proxies=proxy_data)
-        delay = random.uniform(1, 3)
-        time.sleep(delay)
-        if response.status_code == 200:
-            soup = soupify(response)
-            rings_links = soup.find_all('a', class_='product-card__description product-card__link js-product-card-link')
-            print(len(rings_links))
-            for ring_url in rings_links:
-                links = ring_url.get("href")
-                ring_links.append(links)
-                links = str(links)
-                full_url = urljoin(base_url, links)
-                write_to_csv(full_url)
-            logger.info(f"Page {page_number}: Found {len(rings_links)} links")
-        else:
-            print("[ERROR]",response.status_code)
-    except Exception as e:
-        logger.error(f"Page {page_number} error: {str(e)}")
+        ring_links = []
+        try:
+            driver.get("https://www.therealreal.com/")
+            driver.click("div[class='sign-in-sign-up-footer'] a[tabindex='0']:nth-of-type(1)")
+            user_name = input("Enter User Name: ")
+            password = input("Enter Password:")
+            driver.type("input[id='user_email']:nth-of-type(1)", user_name)
+            driver.type('li[aria-label="Password"] input[tabindex="0"]', password)
+            driver.click('li[id="user_submit_action"] input[tabindex="0"]')
+            time.sleep(2)
+        except Exception as e:
+            print("Error ", e)
+        input_query = input("Enter What You Want To Search : ")
+        num_of_pages = int(input("Enter Number of pages you want to crawl : "))
+        driver.type("input[aria-owns='search-listbox-mobile']", input_query)
+        driver.click('[aria-label="Submit search"]')
+        time.sleep(random.uniform(1, 2))
+        for i in range(1, num_of_pages + 1):
 
-    return ring_links
+            print("Page NO", i)
+
+            all_links = driver.select_all(
+                "a[class='product-card__description product-card__link js-product-card-link']")
+
+            print("Crawled Links", len(all_links))
+
+            for j in all_links:
+                link = j.get_attribute('href')
+                link = str(link)
+                full_url = urljoin(base_url, link)
+                write_to_csv(full_url)
+            pagination = driver.select_all(
+                'a[class="pagination__number js-pagination__number plp-pagination__number--suffix"]')
+
+            pagination[1].click()
+            time.sleep(random.uniform(1, 3))
+    except Exception as e:
+        print("ERROR ", e)
+
+    driver.close()
+
 
 if __name__ == "__main__":
-    pages_data = [{"page_number": i} for i in range(1, num_of_pages + 1)]
-    logger.info(f"Starting crawl for {len(pages_data)} pages")
-    result = crawl_rings(pages_data)
-    logger.info(f"Crawling complete. Total links collected: {len(result)}")
-
-
-
-
-
-
-
-
-
-
-
+    scrape_data()
